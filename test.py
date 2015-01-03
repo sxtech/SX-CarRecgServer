5a741bbc0eb39e9c066740a77ed488a2bad7c0d5
SmellyCat <smellycat2014@foxmail.com>
2015-01-04 02:59:48 +0800

    V0.1.1
    增加测试代码

--- test.py
+++ test.py
@@ -1,90 +1,23 @@
-'''This example is a simple WSGI_ script which displays
-the ``Hello World!`` message. To run the script type::
+# -*- coding: utf-8 -*-
 
-    python manage.py
-
-To see all options available type::
-
-    python manage.py -h
-
-.. autofunction:: hello
-
-.. autofunction:: server
-
-.. _WSGI: http://www.python.org/dev/peps/pep-3333/
-'''
-try:
-    from pulsar import MethodNotAllowed
-except ImportError:  # pragma nocover
-    import sys
-    sys.path.append('../../')
-    from pulsar import MethodNotAllowed
-
-from pulsar.apps import wsgi
-import urllib,urlparse
+from httplib2 import Http  
+from urllib import urlencode
+import httplib2
 import json
-import gl
-import threading
-import Queue
-from recg_thread import RecgThread
-
-def hello(environ, start_response):
-    '''The WSGI_ application handler which returns an iterable
-    over the "Hello World!" message.'''
-    #request_body = environ['wsgi.input']
-    
-    if environ['REQUEST_METHOD'] == 'POST' and environ['PATH_INFO']== '/recg':
-        data = request_data(environ["wsgi.input"].read())
-        status = '200 OK'
-        response_headers = [
-            ('Content-type', 'text/plain'),
-            ('Content-Length', str(len(data)))
-        ]
-        start_response(status, response_headers)
-        return iter([data])
-    else:
-        status = '400 Bad Request'
-        response_headers = [
-            ('Content-type', 'text/plain'),
-            ('Content-Length', str(len('Error')))
-        ]
-        start_response(status, response_headers)
-        return iter(['Error'])
 
 
-def server(description=None, **kwargs):
-    '''Create the :class:`.WSGIServer` running :func:`hello`.'''
-    description = description or 'Pulsar Hello World Application'
-    return wsgi.WSGIServer(hello, name='RecgServer', description=description, **kwargs)
+def TestHttpPost():
+  word=u'美国'.encode('utf8')
+  urlstr = 'http://localhost:8060/recg'
+  
+  json_data = json.dumps({'imgurl':'http://localhost/imgareaselect/imgs/6.jpg','coordinates':[1301,1007,2448,2048]})
+  #print json_data
+  data={'key':'sx2767722','info': json_data} 
+  
+  h = httplib2.Http('.cache')
+  response,content = h.request(urlstr, 'POST', urlencode(data), headers={'Content-Type': 'application/x-www-form-urlencoded'})  
 
-def request_data(wsgi_input):
-    data = urllib.unquote_plus(wsgi_input)
-    post_data = urlparse.parse_qs(data,True)
-    if post_data.get('key',None)[0] != 'sx2767722':
-        return json.dumps({'carinfo':None,'msg':'Key Error','code':105})
-    
-    if post_data.get('info',None) == None:
-        return json.dumps({'carinfo':None,'msg':'Json Format Error','code':106})
-
-    info = json.loads(post_data.get('info',[''])[0])
-
-    info['queue'] = Queue.Queue()
-    gl.RECGQUE.put(info)
-    try:
-        recginfo = info['queue'].get(timeout=15)
-        return json.dumps(recginfo)
-    except Queue.Empty:
-        return json.dumps({'carinfo':None,'msg':'Time Out','code':107})
-
-class RecgServer:
-    def __init__(self):
-        gl.RECGQUE = Queue.Queue()
-        for c in range(3):
-            RecgThread(c).start()
-
-    def run(self):
-        server().start()
-
+  print(response,content)
+  
 if __name__ == '__main__':  # pragma nocover
-    rs = RecgServer()
-
+  TestHttpPost()
