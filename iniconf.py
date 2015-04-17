4731e54ddb7ece36118559d347b485458e3dc4df
SmellyCat <smellycat2014@foxmail.com>
2015-04-17 20:33:52 +0800

    V2.0.2
    1.规范了配置文件类

--- iniconf.py
+++ iniconf.py
@@ -5,26 +5,25 @@ import ConfigParser
 class CarRecgSerIni:
 
     def __init__(self, confpath='recg.conf'):
-        self.path = ''
         self.confpath = confpath
         self.cf = ConfigParser.ConfigParser()
         self.cf.read(confpath)
 
     def get_sys_conf(self):
         """获取系统配置参数"""
-        sysconf = {}
-        sysconf['threads'] = self.cf.getint('SYSSET', 'threads')
-        sysconf['port'] = self.cf.getint('SYSSET', 'port')
-        sysconf['selfip'] = self.cf.get('SYSSET', 'selfip')
+        conf = {}
+        conf['threads'] = self.cf.getint('SYSSET', 'threads')
+        conf['port'] = self.cf.getint('SYSSET', 'port')
+        conf['selfip'] = self.cf.get('SYSSET', 'selfip')
 
-        return sysconf
+        return conf
 
     def get_ser_centre_conf(self):
         """获取中心服务参数"""
-        sysconf = {}
-        sysconf['ip'] = self.cf.get('SERCENTRE', 'ip')
+        conf = {}
+        conf['ip'] = self.cf.get('SERCENTRE', 'ip')
 
-        return sysconf
+        return conf
 
 if __name__ == "__main__":
 
