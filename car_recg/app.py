9c0894b928f0c4ca8acaf9e1bc463b50401b8012
SmellyCat <smellycat2014@foxmail.com>
2015-05-31 18:48:57 +0800

    Add flask_httpauth

--- car_recg/app.py
+++ car_recg/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask
 from flask_restful import Api
+from flask_httpauth import HTTPBasicAuth
 from peewee import SqliteDatabase
 
 
@@ -10,3 +11,5 @@ app = Flask(__name__)
 api = Api(app)
 app.config.from_pyfile('config.py')
 db = SqliteDatabase(app.config['DATABASE'], journal_mode='WAL')
+auth = HTTPBasicAuth()
+
