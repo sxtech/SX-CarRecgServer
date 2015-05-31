358a5af7c80f43afd438899d539c6fb9debbe59a
SmellyCat <smellycat2014@foxmail.com>
2015-05-31 22:06:40 +0800

    Remove useless method

--- car_recg/views.py
+++ car_recg/views.py
@@ -44,39 +44,6 @@ class Index(Resource):
         return {'msg': "Welcome to use SX-CarRecgServer %s" % app.config['VERSION']}
 
 
-class RecgList(Resource):
-
-    def post(self):
-        parser = reqparse.RequestParser()
-
-        parser.add_argument('key', type=unicode, required=True,
-                            help='A key value is require', location='json')
-        parser.add_argument('imgurl', type=unicode, required=True,
-                            help='A jpg url is require', location='json')
-        parser.add_argument('coordinates', type=list, required=True,
-                            help='A coordinates array is require',
-                            location='json')
-        args = parser.parse_args()
-
-        if User.get_one(User.key == request.json['key']) is None:
-            return {'status': 401, 'message': 'Unauthorized access'}, 401
-
-        # 回调用的消息队列
-        que = Queue.Queue()
-
-        if gl.RECGQUE.qsize() > app.config['MAXSIZE']:
-            return {'carinfo': None, 'msg': 'Server Is Busy', 'code': 108}, 202
-
-        gl.RECGQUE.put((10, request.json, que))
-
-        try:
-            recginfo = que.get(timeout=5)
-        except Queue.Empty:
-            recginfo = {'carinfo': None, 'msg': 'Time Out', 'code': 107}, 202
-        finally:
-            return recginfo
-
-
 class RecgListApiV1(Resource):
 
     @auth.login_required
@@ -106,35 +73,16 @@ class RecgListApiV1(Resource):
             return recginfo
 
 
-class StateList(Resource):
-
-    def post(self):
-        parser = reqparse.RequestParser()
-
-        parser.add_argument('key', type=unicode, required=True,
-                            help='A key value is require', location='json')
-        args = parser.parse_args()
-
-        if User.get_one(User.key == request.json['key']) is None:
-            return {'status': 401, 'message': 'Unauthorized access'}, 401
-
-        return {'carinfo': None, 'msg': 'State', 'code': 120,
-                'threads': app.config['THREADS'],
-                'qsize': gl.RECGQUE.qsize()}
-
-
 class StateListApiV1(Resource):
 
     @auth.login_required
     def post(self):
-        return {'carinfo': None, 'msg': 'State', 'code': 120,
+        return {'msg': 'State', 'code': 120,
                 'threads': app.config['THREADS'],
                 'qsize': gl.RECGQUE.qsize()}
 
 
 api.add_resource(Index, '/')
-api.add_resource(RecgList, '/recg')
 api.add_resource(RecgListApiV1, '/api/v1/recg')
-api.add_resource(StateList, '/state')
 api.add_resource(StateListApiV1, '/api/v1/state')
 
