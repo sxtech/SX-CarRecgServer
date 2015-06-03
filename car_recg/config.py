63c5adce92d784417445485a7914ae6140b9a5e7
SmellyCat <smellycat2014@foxmail.com>
2015-06-04 07:15:57 +0800

    Remove version server_name

--- car_recg/config.py
+++ car_recg/config.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 DEBUG = False
+# SERVER_NAME = 'CarRecgSer'
+# 数据库名称
 DATABASE = 'carrecgser.db'
 # 识别线程数
 THREADS = 4
-# 允许最大数队列为线程数32倍
-MAXSIZE = 4 * 32
+# 允许最大数队列为线程数8倍
+MAXSIZE = 4 * 16
 # 图片下载文件夹
 IMG_PATH = 'img'
 # 图片截取文件夹
 CROP_PATH = 'crop'
 # 版本号
-VERSION = '2.3.0'
+# VERSION = '2.3.0'
 # 超时
 TIMEOUT = 5
