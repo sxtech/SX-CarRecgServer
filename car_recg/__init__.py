24196efc1444d123f3b20f3b56acad940f6932c1
SmellyCat <smellycat2014@foxmail.com>
2015-06-04 07:29:41 +0800

    Remove User requests_func import

--- car_recg/__init__.py
+++ car_recg/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 from app import app
-from models import User, Users
+from models import Users
 import views
-from requests_func import RequestsFunc
 from recg_ser import RecgServer
 from my_logger import debug_logging, online_logging
 from iniconf import MyIni
@@ -10,7 +9,6 @@ from iniconf import MyIni
 __version__ = '2.3.0'
 
 '''
-车辆识别服务，使用python2.7版本，flask框架。
 返回代码：
 100: Success (识别成功)
 101: Request Error (请求错误)
