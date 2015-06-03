874b118ca2778be0b4f8093b888579b571a01008
SmellyCat <smellycat2014@foxmail.com>
2015-06-04 07:23:09 +0800

    Update recg_ser
    Remove useless method packages
    add log when instance initialize and destroy

--- car_recg/recg_ser.py
+++ car_recg/recg_ser.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
 import os
-import logging
-import Queue
 
-from app import app, db
-from models import User
+from app import app, logger
 import gl
 from recg_thread import RecgThread
 from iniconf import MyIni
-
-
-logger = logging.getLogger('root')
 
 
 class RecgServer:
@@ -18,14 +12,8 @@ class RecgServer:
         # 配置文件
         self.ini = MyIni()
         self.sysini = self.ini.get_sys_conf()
-        self.centreini = self.ini.get_ser_centre_conf()
-        # 创建sqlite对象
-        self.db = db
-        # 识别消息优先队列
-        gl.RECGQUE = Queue.PriorityQueue()
 
         app.config['THREADS'] = self.sysini['threads']
-
         # 创建图片下载文件夹
         if not os.path.exists(app.config['IMG_PATH']):
             os.makedirs(app.config['IMG_PATH'])
@@ -33,19 +21,14 @@ class RecgServer:
         if not os.path.exists(app.config['CROP_PATH']):
             os.makedirs(app.config['CROP_PATH'])
 
+        logger.info('Recg server start')
+
     def __del__(self):
-        gl.IS_QUIT = True
         del self.ini
+        gl.IS_QUIT = True
+        logger.info('Recg server quit')
 
     def main(self):
-        self.db.connect()
-        for user in User.select():
-            gl.KEYSDICT[user.key] = {'priority': user.priority,
-                                     'multiple': user.multiple}
-        self.db.close()
-        # 权限队列数
-        gl.P_SIZE = {10: 0, 20: 0, 30: 0}
         # 创建车辆识别线程
-        for c in range(app.config['THREADS']):
-            RecgThread(c).start()
-
+        for i in range(app.config['THREADS']):
+            RecgThread(i).start()
