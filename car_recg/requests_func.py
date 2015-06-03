25dde1faeae919555726ac3eb0b5afc4c038d8f4
SmellyCat <smellycat2014@foxmail.com>
2015-06-04 07:24:29 +0800

    Update Requestfunc class change to function

--- car_recg/requests_func.py
+++ car_recg/requests_func.py
@@ -4,21 +4,18 @@ import shutil
 import requests
 
 
-class RequestsFunc:
-    """HTTP客服端类"""
-    def get_url_img(self, url, path):
-        """根据URL地址抓图到本地文件"""
-        r = requests.get(url, stream=True)
+def get_url_img(url, path):
+    """根据URL地址抓图到本地文件"""
+    r = requests.get(url, stream=True)
 
-        if r.status_code == 200:
-            with open(path, 'wb') as f:
-                r.raw.decode_content = True
-                shutil.copyfileobj(r.raw, f)
-        # 非200响应,抛出异常
-        r.raise_for_status()
+    if r.status_code == 200:
+        with open(path, 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+    # 非200响应,抛出异常
+    r.raise_for_status()
 
-    def send_post(self, url, send_data):
-        """POST请求"""
-        return requests.post(url, data=send_data,
-                          headers={'content-type':'application/json'})
 
+def send_post(url, data, headers={'content-type': 'application/json'}):
+    """POST请求"""
+    return requests.post(url, data=data, headers=headers)
