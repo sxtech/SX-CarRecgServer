cf244d74c2c93f27301183b7af2f357b67e1da05
SmellyCat <smellycat2014@foxmail.com>
2015-06-07 03:27:58 +0800

    Update config.py make a class

--- car_recg/config.py
+++ car_recg/config.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
-DEBUG = False
-# SERVER_NAME = 'CarRecgSer'
-# 数据库名称
-DATABASE = 'carrecgser.db'
-# 识别线程数
-THREADS = 4
-# 允许最大数队列为线程数8倍
-MAXSIZE = 4 * 16
-# 图片下载文件夹
-IMG_PATH = 'img'
-# 图片截取文件夹
-CROP_PATH = 'crop'
-# 版本号
-# VERSION = '2.3.0'
-# 超时
-TIMEOUT = 5
+import os
+
+
+class Config(object):
+    DEBUG = True
+    BASEDIR = os.path.abspath(os.path.dirname(__file__))
+    # 主机IP
+    HOST = '0.0.0.0'
+    # 数据库名称
+    DATABASE = 'carrecgser.db'
+    # 识别线程数
+    THREADS = 4
+    # 允许最大数队列为线程数16倍
+    MAXSIZE = THREADS * 16
+    PORT = '8060'
+    # 图片下载文件夹
+    IMG_PATH = 'img'
+    # 图片截取文件夹
+    CROP_PATH = 'crop'
+    # 超时
+    TIMEOUT = 5
+
+class Develop(Config):
+    pass
+
+class Production(Config):
+    DEBUG = False
+
