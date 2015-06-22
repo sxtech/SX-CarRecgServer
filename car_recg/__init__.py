87a07fdae771d05394f4c0786b6e6e79af188790
SmellyCat <smellycat2014@foxmail.com>
2015-06-23 02:11:53 +0800

    Add http status code to init

--- car_recg/__init__.py
+++ car_recg/__init__.py
@@ -9,16 +9,14 @@ from iniconf import MyIni
 __version__ = '2.3.0'
 
 '''
-返回代码：
-100: Success (识别成功)
-101: Request Error (请求错误)
-102: Recognise Error (识别错误)
-103: Url Error (图片url路径错误)
-104: Unknow Error (未知错误)
-105: Key Error (用户密钥错误)
-106: Json Format Error (json格式错误)
-107: Time Out (超时)
-108: Server Is Busy (服务繁忙)
-109: Recg Server Error (识别服务错误)
-#110: No Info Parameter (POST缺少info参数)
+HTTP Status Code
+200: OK
+201: Created
+400: Bad Request
+401: Unauthorized
+403: Forbidden
+408: Request Timeout
+422: Unprocessable Entity
+449: Retry With
+500: Internal Server Error
 '''
