48021d999ed423d72554a111b648a370d683836f
SmellyCat <smellycat2014@foxmail.com>
2015-07-04 10:51:19 +0800

    Update test

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-from car_recg import User, Users
-from car_recg import RequestsFunc
+# -*- coding: utf-8 -*-
+
+from car_recg import Users, Recglist
+from car_recg.carrecg import CarRecgEngine
+#from car_recg import RequestsFunc
 import json
 
 def models_test():
@@ -9,6 +12,28 @@ def models_test():
     u = Users.get_one(Users.username == 'kakou')
     print u.password
 
+def create_table():
+    Users.create_table(True)
+    Recglist.create_table(True)
+
+def save_test():
+    r = Recglist.create(timestamp=123,
+                          imgurl='http://www.baidu.com',
+                          recginfo='{"海贼王":123}')
+    print r.last_insert_id()
+
+def recg_test():
+    r = CarRecgEngine()
+    print r.imgrecg('test.jpg', [])
+    del r
+
+def get_test():
+    for users in Users.select().where(Users.banned == 0):
+        print users.id
 
 if __name__ == '__main__':
-    models_test()
+    #models_test()
+    #create_table()
+    #save_test()
+    #recg_test()
+    get_test()
