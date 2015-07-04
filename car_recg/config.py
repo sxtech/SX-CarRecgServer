269d246888adc5e6c505cb9f69e8711bc77f930d
SmellyCat <smellycat2014@foxmail.com>
2015-07-04 10:50:26 +0800

    Update config add UPLOAD_PATH

--- car_recg/config.py
+++ car_recg/config.py
@@ -6,28 +6,30 @@ import Queue
 class Config(object):
     DEBUG = True
     BASEDIR = os.path.abspath(os.path.dirname(__file__))
-    # 主机IP
+    # 主机IP string
     HOST = '0.0.0.0'
-    # 端口
+    # 端口 string
     PORT = '8060'
-    # 数据库名称
+    # 数据库名称 string
     DATABASE = 'carrecgser.db'
-    # 识别线程数
+    # 识别线程数 int
     THREADS = 4
-    # 允许最大数队列为线程数16倍
+    # 允许最大数队列为线程数16倍 int
     MAXSIZE = THREADS * 16
-    # 图片下载文件夹
+    # 图片下载文件夹 string
     IMG_PATH = 'img'
-    # 图片截取文件夹
+    # 图片截取文件夹 string
     CROP_PATH = 'crop'
-    # 超时
+    # 超时 int
     TIMEOUT = 5
-    # 识别优先队列
+    # 识别优先队列 object
     RECGQUE = Queue.PriorityQueue()
-    # 退出标记
+    # 退出标记 bool
     IS_QUIT = False
-    # 用户字典
+    # 用户字典 dict
     USER = {}
+    # 上传文件保存路径 string
+    UPLOAD_PATH = 'upload'
 
 
 class Develop(Config):
